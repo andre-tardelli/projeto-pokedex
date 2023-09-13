9cb2b6c6a50cb325adef98da33350263fa7abb0f
André Tardelli <andre.ltardelli@gmail.com>
2023-09-12 21:57:05 -0300

    Editando os types e as imgs do projeto

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,18 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset=0, limit=10) => {
-  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-  return fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .catch((error) => console.log.error(error))
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((reponse) => reponse.json())
 }
+
+pokeApi.getPokemons = (offset=0, limit=5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests)) 
+    .then((pokemonDetails) => pokemonDetails)
+}
+
